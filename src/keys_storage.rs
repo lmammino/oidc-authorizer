d0589644a2487766d2e5af14686810f26f6b3315
Luciano Mammino <lucianomammino@gmail.com>
2023-10-29 17:44:00 +0000

    fix: bug with loading keys already in cache

--- src/keys_storage.rs
+++ src/keys_storage.rs
@@ -37,17 +37,22 @@ impl KeysStorage {
     pub async fn get(&self, key_id: &str) -> Result<DecodingKey, KeysStorageError> {
         let read_guard = self.storage.read().await;
         let maybe_key = read_guard.0.get(key_id);
-        let should_refresh = read_guard.1 + self.min_refresh_rate < Utc::now();
-        if maybe_key.is_none() && should_refresh {
-            drop(read_guard);
-            self.refresh().await?;
-            let read_guard = self.storage.read().await;
-            let maybe_key = read_guard.0.get(key_id);
-            if let Some(key) = maybe_key {
-                return Ok(key.clone());
+        match maybe_key {
+            Some(key) => return Ok(key.clone()),
+            None => {
+                let should_refresh = read_guard.1 + self.min_refresh_rate < Utc::now();
+                if should_refresh {
+                    drop(read_guard);
+                    self.refresh().await?;
+                    let read_guard = self.storage.read().await;
+                    let maybe_key = read_guard.0.get(key_id);
+                    if let Some(key) = maybe_key {
+                        return Ok(key.clone());
+                    }
+                    drop(read_guard);
+                }
             }
-            drop(read_guard);
-        }
+        };
 
         Err(KeysStorageError::KeyNotFound(key_id.to_string()))
     }
@@ -110,6 +115,49 @@ mod tests {
         let keys_cache = KeysStorage::new(jwks_uri.clone(), min_refresh_rate);
         let key_result = keys_cache.get(key_id).await;
         assert!(key_result.is_ok());
+        jwks_mock.assert_hits(1);
+
+        // if it reads the key again it should be taken straight away from cache
+        let key_result = keys_cache.get(key_id).await;
+        assert!(key_result.is_ok());
+        jwks_mock.assert_hits(1); // no new hits
+    }
+
+    #[tokio::test]
+    async fn it_should_return_an_error_if_the_key_is_not_found() {
+        let server = MockServer::start();
+        let jwks_mock = server.mock(|when, then| {
+            when.method(GET)
+                .path("/");
+            then.status(200)
+                .header("content-type", "application/json")
+                .body(r#"
+                    {
+                        "keys":[
+                            {
+                                "kty":"RSA",
+                                "n":"0TF4RX87dOllFp12D8IZvSoJyp8D4IZ3JmlVG7Au2GOSp1WcrAqjyq3Gk-a_1tT31FHCLVqjH9vXE8g1sXika4mp8YCWyMfjT3KsfrciI_Fw-nBCawnqewBDcBo4cvBgTjHNBjcjGNr0U_4eCZPjP8pwqw6HrRgHf-ypNmtgWG6_2EaK-tOJtnNgGRtCYGZdqMDfKLDuqzU5-gT2ejt9P1kNAvFMMUm4dTOK-vJ7jwGKWZEzupHBlHMqu4K4IRoFbVr2XsAzV5YQ0u_r26NVtQTDUdTp9ixhexUp0eXye6m3uMklqUOHJbiqNjmH2ye4yXVJI0w6BFOeXXlwyR6slw",
+                                "e":"AQAB",
+                                "alg":"RS256",
+                                "kid":"test/keys/rs256/public",
+                                "use":"sig"
+                            }
+                        ]
+                    }            
+                "#);
+        });
+
+        let key_id = "invalid";
+        let jwks_uri = Url::parse(server.url("/").as_str()).unwrap();
+        let min_refresh_rate = Duration::seconds(60);
+        let keys_cache = KeysStorage::new(jwks_uri.clone(), min_refresh_rate);
+        let key_result = keys_cache.get(key_id).await;
+        if let Err(KeysStorageError::KeyNotFound(_)) = key_result {
+            // expected
+        } else {
+            panic!("Expected a KeyNotFound error");
+        }
+
         jwks_mock.assert();
     }
 }
